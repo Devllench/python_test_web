f68f0a9d180072c89921ee1f02d884e6c3c88af4
Lenya Sychev <lenya2012@mail.ru>
2020-04-08 20:22:10 +0300

    Добавлена проверка  в тесты на наличие группы для удаления и модификации, в случае если группы нет, она создается и затем удаяется

--- tests/test_UpdateGroup.py
+++ tests/test_UpdateGroup.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
-from model.сlass_test_group import Group, Auth
+from model.сlass_test_group import Group
 
 
 def test_update_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(group_name="update tested3up", group_header="update test head rec3up", group_footer="update test foot rec3up"))
     app.group.update(Group(group_name="tested3up", group_header="test head rec3up", group_footer="test foot rec3up"))
-    app.open_home_page()
+    #app.open_home_page()
 
 
 def test_update_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(group_name="update tested3up"))
     app.group.update(Group(group_name="New testedname"))
-    app.open_home_page()
+
+    #app.open_home_page()
 
 
 def test_update_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(group_header="update tested3up"))
     app.group.update(Group(group_header="New testedheader"))
-    app.open_home_page()
+    #app.open_home_page()
